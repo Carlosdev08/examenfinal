4878ec56275bd0cc6c26e8be4da2f5b3477bbe79
Carlos Del Jesus <144354364+Carlosdev08@users.noreply.github.com>
2024-03-01 04:20:44 +0100

    add card

--- src/app/cardgenerator/page.js
+++ src/app/cardgenerator/page.js
@@ -1,9 +1,10 @@
+import CardGenerator from 'examenfinal/components/CardGenerator'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-        
+    <CardGenerator />    
     </div>
   )
 }
--- src/components/CardGenerator.jsx
+++ src/components/CardGenerator.jsx
@@ -1,7 +1,8 @@
 
-
 "use client"
 import React, {useState} from 'react'
+import Image from 'next/image'
+import { UserRound } from 'lucide-react'
 
 const CardGenerator = () => {
 
@@ -10,6 +11,7 @@ const CardGenerator = () => {
     const [location, setLocation] = useState('');
     const [card, setCard] = useState(null);
     const [image, setImage] = useState('');
+    
 
     const generateCard = () => {
         
@@ -22,13 +24,15 @@ const CardGenerator = () => {
             reader.onload = (e) => {
                 setImage(e.target.result);
             };
+            reader.readAsDataURL(file);
         }
     }
   return (
     <div className=" mx-auto p-4 bg-origin-border font-outfit">
+        <h2 className='text-center bg-purple-700 font-outfit font-semibold text-white p-5' >ID Card Generator.</h2>
     <div className="flex justify-between border">
       <div className="w-1/2 p-4">
-        <h2 className="text-lg font-semibold mb-4 text-center flex items-center justify-center">ID Card Generator.
+        <h2 className="text-lg font-semibold mb-4 text-center flex items-center justify-center">Input Form
 </h2>
         <div className="mb-2">
           <label className="block mb-1">Enter Name:</label>
@@ -62,21 +66,31 @@ const CardGenerator = () => {
         </div>  
         <button
           onClick={generateCard}
-          className="w-full bg-purple-600  p-2 rounded"
+          className="w-32 text-white font-outfit font-semibold bg-purple-600  p-2 rounded"
         >
           Generate Card
         </button>
       </div>
-      {card && (
-        <div className="w-1/2 p-4">
-          <h2 className="text-lg font-semibold mb-4">Generated Card</h2>
-          <div className="border-2 p-4">
-            <p><strong>Name:</strong> {card.name}</p>
-            <p><strong>College Name:</strong> {card.collegeName}</p>
-            <p><strong>Location:</strong> {card.location}</p>
-          </div>
-        </div>
-      )}
+{card && (
+  <div className="w-1/2 p-4 border border-collapse border-spacing-11  bg-{#cccc} ">
+    <h2 className="text-lg font-semibold mb-10 text-center">Generated Card</h2>
+    <div className="border-2 p-4 flex flex-row items-center">
+      <div style={{ 
+        backgroundImage: `url(${card.image})`, 
+        backgroundSize: 'cover', 
+        width: '200px', 
+        height: '200px' 
+      }}>
+        <UserRound size={100} color="bg-transpàrent" />
+      </div>
+      <div className="ml-4">
+        <p><strong>Name:</strong> {card.name}</p>
+        <p><strong>College Name:</strong> {card.collegeName}</p>
+        <p><strong>Location:</strong> {card.location}</p>
+      </div>
+    </div>
+  </div>
+)}
     </div>
   </div>
 );
