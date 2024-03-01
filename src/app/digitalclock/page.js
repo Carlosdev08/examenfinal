6bc5d18714e90e884785ef8dc4e1a34ee2b22222
Carlos Del Jesus <144354364+Carlosdev08@users.noreply.github.com>
2024-03-01 04:31:35 +0100

    finalized digitalclock

---
+++ src/app/digitalclock/page.js
@@ -0,0 +1,12 @@
+import DigitalClock from 'examenfinal/components/DigitalClock'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <DigitalClock />
+    </>
+  )
+}
+
+export default page
